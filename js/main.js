559cafab44678c00cdf3cfe0f55fb40586edd671
Vladimir Istomin <55831490+vvistmn@users.noreply.github.com>
2019-10-05 16:35:31 +0400

    1.42

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        dots:false,
+        navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1.5
+            }
+        }
+    });
+});
