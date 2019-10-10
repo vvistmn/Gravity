9f6fda864bdc2de869bbcce32c72db57495130b7
Vladimir Istomin <55831490+vvistmn@users.noreply.github.com>
2019-10-10 18:50:20 +0400

    Сделал второй слайдер

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('.owl-carousel').owlCarousel({
+    $('.works-carousel').owlCarousel({
         loop:true,
         margin:30,
         nav:true,
@@ -14,6 +14,24 @@ $(function() {
             },
             1000:{
                 items:1.5
+            }
+        }
+    });
+    $('.contributor-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        dots:false,
+        navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
             }
         }
     });
